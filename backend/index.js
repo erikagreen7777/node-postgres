71b579cce045dcbc66dad23ee0adb31407b602fd
Erika Green <erikagreen7777@gmail.com>
2025-02-26 10:25:42 -0500

    adjust routes

--- backend/index.js
+++ backend/index.js
@@ -4,10 +4,15 @@ const app = express();
 const port = 3001;
 
 const meal_minder_model = require("db/mealMinderModel");
+const createUser = require("./routes/createUser");
+const getUsers = require("./routes/getUsers");
+const deleteUser = require("./routes/deleteUser");
+const updateUser = require("./routes/updateUser");
 
 app.use(express.json());
+
 app.use(function (req, res, next) {
-  res.setHeader("Access-Control-Allow-Origin", "http://localhost:5173");
+  res.setHeader("Access-Control-Allow-Origin", "http://localhost:3000");
   res.setHeader("Access-Control-Allow-Methods", "GET,POST,PUT,DELETE,OPTIONS");
   res.setHeader(
     "Access-Control-Allow-Headers",
@@ -16,9 +21,8 @@ app.use(function (req, res, next) {
   next();
 });
 
-app.get("/", (req, res) => {
-  meal_minder_model
-    .getUsers()
+app.get("/users", (req, res) => {
+  getUsers()
     .then((response) => {
       res.status(200).send(response);
     })
@@ -28,6 +32,38 @@ app.get("/", (req, res) => {
 });
 //   res.status(200).send("Hello World!");
 // });
+
+app.post("/users", (req, res) => {
+  createUser(req.body)
+    .then((response) => {
+      res.status(200).send(response);
+    })
+    .catch((error) => {
+      res.status(500).send(error);
+    });
+});
+
+app.delete("/users/:id", (req, res) => {
+  deleteUser(req.params.id)
+    .then((response) => {
+      res.status(200).send(response);
+    })
+    .catch((error) => {
+      res.status(500).send(error);
+    });
+});
+
+app.put("/users/:id", (req, res) => {
+  const id = req.params.id;
+  const body = req.body;
+  updateUser(id, body)
+    .then((response) => {
+      res.status(200).send(response);
+    })
+    .catch((error) => {
+      res.status(500).send(error);
+    });
+});
 
 app.listen(port, () => {
   console.log(`App running on port ${port}.`);
