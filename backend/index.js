e67ff04310129cc90e9d06f5fe199908f28282e0
Erika Green <erikagreen7777@gmail.com>
2025-02-26 13:32:12 -0500

    connecting FE to BE

--- backend/index.js
+++ backend/index.js
@@ -1,18 +1,23 @@
 require("dotenv").config();
+const fs = require("fs");
+const https = require("https");
 const express = require("express");
+
 const app = express();
 const port = 3001;
 
-const meal_minder_model = require("db/mealMinderModel");
-const createUser = require("./routes/createUser");
-const getUsers = require("./routes/getUsers");
-const deleteUser = require("./routes/deleteUser");
-const updateUser = require("./routes/updateUser");
+// Load SSL certificate
+const options = {
+  key: fs.readFileSync(process.env.SSL_KEY),
+  cert: fs.readFileSync(process.env.SSL_CERT),
+};
 
+// Middleware
 app.use(express.json());
 
+// CORS Headers
 app.use(function (req, res, next) {
-  res.setHeader("Access-Control-Allow-Origin", "http://localhost:3000");
+  res.setHeader("Access-Control-Allow-Origin", "https://localhost:3000");
   res.setHeader("Access-Control-Allow-Methods", "GET,POST,PUT,DELETE,OPTIONS");
   res.setHeader(
     "Access-Control-Allow-Headers",
@@ -21,50 +26,108 @@ app.use(function (req, res, next) {
   next();
 });
 
+// Routes
+const createUser = require("./routes/createUser");
+const getUsers = require("./routes/getUsers");
+const deleteUser = require("./routes/deleteUser");
+const updateUser = require("./routes/updateUser");
+
 app.get("/users", (req, res) => {
   getUsers()
-    .then((response) => {
-      res.status(200).send(response);
-    })
-    .catch((error) => {
-      res.status(500).send(error);
-    });
+    .then((response) => res.status(200).send(response))
+    .catch((error) => res.status(500).send(error));
 });
-//   res.status(200).send("Hello World!");
-// });
 
 app.post("/users", (req, res) => {
   createUser(req.body)
-    .then((response) => {
-      res.status(200).send(response);
-    })
-    .catch((error) => {
-      res.status(500).send(error);
-    });
+    .then((response) => res.status(200).send(response))
+    .catch((error) => res.status(500).send(error));
 });
 
 app.delete("/users/:id", (req, res) => {
   deleteUser(req.params.id)
-    .then((response) => {
-      res.status(200).send(response);
-    })
-    .catch((error) => {
-      res.status(500).send(error);
-    });
+    .then((response) => res.status(200).send(response))
+    .catch((error) => res.status(500).send(error));
 });
 
 app.put("/users/:id", (req, res) => {
-  const id = req.params.id;
-  const body = req.body;
-  updateUser(id, body)
-    .then((response) => {
-      res.status(200).send(response);
-    })
-    .catch((error) => {
-      res.status(500).send(error);
-    });
+  updateUser(req.params.id, req.body)
+    .then((response) => res.status(200).send(response))
+    .catch((error) => res.status(500).send(error));
 });
 
-app.listen(port, () => {
-  console.log(`App running on port ${port}.`);
+// Start HTTPS Server
+https.createServer(options, app).listen(port, () => {
+  console.log(`Server running at https://localhost:${port}`);
 });
+
+// require("dotenv").config();
+// const express = require("express");
+// const app = express();
+// const port = 3001;
+
+// // const meal_minder_model = require("db/mealMinderModel");
+// const createUser = require("./routes/createUser");
+// const getUsers = require("./routes/getUsers");
+// const deleteUser = require("./routes/deleteUser");
+// const updateUser = require("./routes/updateUser");
+
+// app.use(express.json());
+
+// app.use(function (req, res, next) {
+//   res.setHeader("Access-Control-Allow-Origin", "https://localhost:3000");
+//   res.setHeader("Access-Control-Allow-Methods", "GET,POST,PUT,DELETE,OPTIONS");
+//   res.setHeader(
+//     "Access-Control-Allow-Headers",
+//     "Content-Type, Access-Control-Allow-Headers"
+//   );
+//   next();
+// });
+
+// app.get("/users", (req, res) => {
+//   getUsers()
+//     .then((response) => {
+//       res.status(200).send(response);
+//     })
+//     .catch((error) => {
+//       res.status(500).send(error);
+//     });
+// });
+// //   res.status(200).send("Hello World!");
+// // });
+
+// app.post("/users", (req, res) => {
+//   createUser(req.body)
+//     .then((response) => {
+//       res.status(200).send(response);
+//     })
+//     .catch((error) => {
+//       res.status(500).send(error);
+//     });
+// });
+
+// app.delete("/users/:id", (req, res) => {
+//   deleteUser(req.params.id)
+//     .then((response) => {
+//       res.status(200).send(response);
+//     })
+//     .catch((error) => {
+//       res.status(500).send(error);
+//     });
+// });
+
+// app.put("/users/:id", (req, res) => {
+//   const id = req.params.id;
+//   const body = req.body;
+//   updateUser(id, body)
+//     .then((response) => {
+//       res.status(200).send(response);
+//     })
+//     .catch((error) => {
+//       res.status(500).send(error);
+//     });
+// });
+
+// app.listen(port, () => {
+//   console.log(`App running on port ${port}.`);
+// });
